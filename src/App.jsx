20ae56904fc4fad8d85e905a7febb946c35085ff
Egor Cravcenco <cregordudes@gmail.com>
2024-05-20 17:35:25 +0300

    Implement Tg-changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,39 @@ import AppRouter from "./components/AppRouter";
 import WebApp from "@twa-dev/sdk";
 
 function App() {
-   //useEffect(() => {
-   //   if (WebApp.initData) {
-   //      WebApp.sendData("App started");
-   //      WebApp.sendData(initData);
+   useEffect(() => {
+      WebApp.expand();
+      console.log(WebApp?.initDataUnsafe?.user);
+      WebApp.sendData(WebApp?.platform);
 
-   //      if (WebApp.WebAppInitData) {
-   //         console.log(WebApp.WebAppInitData);
-   //         WebApp.sendData(WebApp.WebAppInitData);
-   //         WebApp.
-   //      }
+      //   if (WebApp.initData) {
+      //      WebApp.sendData("App started");
+      //      WebApp.sendData(initData);
 
-   //      if (WebApp.WebAppUser) {
-   //         console.log(WebApp.WebAppUser);
-   //         WebApp.sendData(WebApp.WebAppUser);
-   //      }
+      //      if (WebApp.WebAppInitData) {
+      //         console.log(WebApp.WebAppInitData);
+      //         WebApp.sendData(WebApp.WebAppInitData);
+      //         WebApp.
+      //      }
 
-   //      WebApp.onEvent("mainButtonClicked", () => {
-   //         WebApp.sendData("mainButtonClicked");
-   //      });
+      //      if (WebApp.WebAppUser) {
+      //         console.log(WebApp.WebAppUser);
+      //         WebApp.sendData(WebApp.WebAppUser);
+      //      }
 
-   //      WebApp.onEvent("message", async (msg) => {
-   //         const chatId = msg.chat.id;
-   //         const text = msg.text;
-   //         if (text) {
-   //            await WebApp.sendText(chatId, "Hello!");
-   //         }
-   //      });
-   //   }
-   //}, []);
+      //      WebApp.onEvent("mainButtonClicked", () => {
+      //         WebApp.sendData("mainButtonClicked");
+      //      });
+
+      //      WebApp.onEvent("message", async (msg) => {
+      //         const chatId = msg.chat.id;
+      //         const text = msg.text;
+      //         if (text) {
+      //            await WebApp.sendText(chatId, "Hello!");
+      //         }
+      //      });
+      //   }
+   }, []);
 
    console.log(WebApp);
    return <AppRouter />;
