b294b64ea4afc8addedfac81df293d256190ab0f
Egor Cravcenco <cregordudes@gmail.com>
2024-05-20 16:01:50 +0300

    Comment out tg integration

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,35 @@ import AppRouter from "./components/AppRouter";
 import WebApp from "@twa-dev/sdk";
 
 function App() {
-   useEffect(() => {
-      if (WebApp.initData) {
-         WebApp.sendData("App started");
-         WebApp.sendData(initData);
+   //useEffect(() => {
+   //   if (WebApp.initData) {
+   //      WebApp.sendData("App started");
+   //      WebApp.sendData(initData);
 
-         if (WebApp.WebAppInitData) {
-            console.log(WebApp.WebAppInitData);
-            WebApp.sendData(WebApp.WebAppInitData);
-         }
+   //      if (WebApp.WebAppInitData) {
+   //         console.log(WebApp.WebAppInitData);
+   //         WebApp.sendData(WebApp.WebAppInitData);
+   //         WebApp.
+   //      }
 
-         if (WebApp.WebAppUser) {
-            console.log(WebApp.WebAppUser);
-            WebApp.sendData(WebApp.WebAppUser);
-         }
+   //      if (WebApp.WebAppUser) {
+   //         console.log(WebApp.WebAppUser);
+   //         WebApp.sendData(WebApp.WebAppUser);
+   //      }
 
-         WebApp.onEvent("mainButtonClicked", () => {
-            WebApp.sendData("mainButtonClicked");
-         });
+   //      WebApp.onEvent("mainButtonClicked", () => {
+   //         WebApp.sendData("mainButtonClicked");
+   //      });
 
-         WebApp.onEvent("message", async (msg) => {
-            const chatId = msg.chat.id;
-            const text = msg.text;
-            if (text) {
-               await WebApp.sendText(chatId, "Hello!");
-            }
-         });
-      }
-   }, []);
+   //      WebApp.onEvent("message", async (msg) => {
+   //         const chatId = msg.chat.id;
+   //         const text = msg.text;
+   //         if (text) {
+   //            await WebApp.sendText(chatId, "Hello!");
+   //         }
+   //      });
+   //   }
+   //}, []);
 
    console.log(WebApp);
    return <AppRouter />;
