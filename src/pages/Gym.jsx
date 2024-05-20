089cc7e67378d7582e896bc97dd62c09527a49a7
Egor Cravcenco <cregordudes@gmail.com>
2024-05-20 22:16:40 +0300

    Add Lazy loading to Gym page

--- src/pages/Gym.jsx
+++ src/pages/Gym.jsx
@@ -1,15 +1,19 @@
 import GymImage from "../assets/Gym.png";
 import GymCharacter from "../assets/characterGym.png";
+import WebApp from "@twa-dev/sdk";
+import TouchCounter from "../components/TouchCounter";
 
 const Gym = () => {
    return (
       <div className="relative">
-         <img src={GymImage} alt="gym" />
-         <img
-            src={GymCharacter}
-            alt="chad"
-            className="absolute bottom-56 left-36"
-         />
+         <TouchCounter>
+            <img src={GymImage} alt="gym" />
+            <img
+               src={GymCharacter}
+               alt="chad"
+               className="absolute bottom-56 left-36"
+            />
+         </TouchCounter>
       </div>
    );
 };
